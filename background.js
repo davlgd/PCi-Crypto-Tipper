60c564697ebf4a2d96fdf94e1abea06f9485fa2d
LEGRAND david <david@nextinpact.com>
2014-06-21 10:39:27 +0200

    Mise à jour du fichier de background

--- background.js
+++ background.js
@@ -1,8 +1,7 @@
-var urlCommentsAJAX = "http://www.pcinpact.com/Actu/SetCommentsRead";
+var urlCommentsAJAX = "http://www.nextinpact.com/Actu/SetCommentsRead";
 
 chrome.webRequest.onCompleted.addListener(function (e) {
     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {greeting: "commentsUpdate"});
     });
 }, {urls: [urlCommentsAJAX]});
-
